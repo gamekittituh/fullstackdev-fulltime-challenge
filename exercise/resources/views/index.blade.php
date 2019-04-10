12c3284e7e3798536783a09fd138dcfa06339461
gamekittituh <gcommax@gmail.com>
2019-04-10 16:50:01 +0700

    install laravel

---
+++ exercise/resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+@section('title','home')
+@section('content')
+<div class="row">
+    <!-- for loop -->
+    <div class="col-xs-6 col-sm-3">
+        <!-- click show module -->
+        <div class="box-locker">
+            <div class="text-center">
+                <h4>S</h4>
+                <span class="text-muted">#1</span>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3">
+        <!-- click show module -->
+        <div class="box-locker">
+            <div class="text-center">
+                <h4>S</h4>
+                <span class="text-muted">#1</span>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ exercise/resources/views/layouts/master.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') | Coin Locker</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+    @yield('header-script')
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse navbar-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Coin Locker</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    @yield('footer-script')
+</body>
+
+</html>
